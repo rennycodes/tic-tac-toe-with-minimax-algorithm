1db27890cf8c93436b3d6099738df3dfed2f6595
Renny <rennyemmanuel8@gmail.com>
2024-01-13 08:08:46 -1200

    fixed: fatal index file error

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -17,3 +17,14 @@ def display_board(board):
     print(board[3] + " | " + board[4] + " | " + board[5])
     print("----------")
     print(board[6] + " | " + board[7] + " | " + board[8])
+
+# Getting player input
+def get_player_input(board):
+    usr_inp = int(input("choose a board space 1-9: "))
+    if usr_inp >= 1 and usr_inp <= 9 and board[usr_inp-1] == "-":
+        board[usr_inp-1] = current_player
+    else:
+        print("Oh noo, that spot is already taken(*_*)")
+
+display_board(board)
+get_player_input(board)
