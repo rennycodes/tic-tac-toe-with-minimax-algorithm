cf9f0ec60b3d1e1878a539eb2ff3ffa7d81e73cd
Renny <rennyemmanuel8@gmail.com>
2024-01-12 19:44:43 -1200

    added: variables and game board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,3 +1,11 @@
 import random
 
-#
+# Variables
+game_running = True
+winner = None
+current_player = "X"
+
+# Creating the game board
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
