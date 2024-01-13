b073f47dbca6ee76ab5eca9249711d946c490957
Renny <rennyemmanuel8@gmail.com>
2024-01-12 19:48:07 -1200

    displayed: board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -9,3 +9,12 @@ current_player = "X"
 board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
+
+# Displaying the game board
+def display_board(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("----------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("----------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print("----------")
