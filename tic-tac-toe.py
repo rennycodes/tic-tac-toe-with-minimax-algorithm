ebc18a15473378e40c92847fe6a52eaeba324bd0
Renny <rennyemmanuel8@gmail.com>
2024-01-14 17:28:14 -1200

    adjusted: board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,9 +6,9 @@ board = {1: ' ', 2: ' ', 3: ' ',
 # Displaying the game board
 def display_board(board):
     print(board[1] + " | " + board[2] + " | " + board[3])
-    print("----------")
+    print("--+---+--")
     print(board[4] + " | " + board[5] + " | " + board[6])
-    print("----------")
+    print("--+---+--")
     print(board[7] + " | " + board[8] + " | " + board[9])
 display_board(board)
 
