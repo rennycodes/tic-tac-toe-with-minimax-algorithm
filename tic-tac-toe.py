cd1ca447c33405ef27c4c23b367d3a5411f1609d
Renny <rennyemmanuel8@gmail.com>
2024-01-15 20:16:41 -1200

    trying stuff

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -134,7 +134,7 @@ def make_best_move(board):
 # Random computer
 def random_computer(board):
     while current_player == "O":
-        position = make_best_move(board)
+        position = make_move(board)
         if board[position] == "-":
             board[position] = "O"
             switch_player()
