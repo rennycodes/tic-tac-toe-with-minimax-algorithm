e4a31c7faa4a382044906f6e4e9ee4a5356cd68a
Renny <rennyemmanuel8@gmail.com>
2024-01-16 23:04:01 -1200

    working more

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -10,7 +10,7 @@ def display_board(board):
     print(board[4] + " | " + board[5] + " | " + board[6])
     print("--+---+--")
     print(board[7] + " | " + board[8] + " | " + board[9])
-display_board(board)
+
 
 
 
