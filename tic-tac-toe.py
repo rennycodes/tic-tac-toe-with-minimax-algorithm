c16439a97cb3ba2be4f3be1cde6ec714da4c08ac
Renny <rennyemmanuel8@gmail.com>
2024-01-17 00:14:21 -1200

    added: check win

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -40,13 +40,31 @@ def insert_letter(letter, position):
         return
 
 def check_win():
-    pass
+    if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+        return True
+    
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+        return True
+    
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+        return True
+    
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+        return True
+    
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+        return True
 
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+        return True
 
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+        return True
 
+    elif (board[3] == board[5] and board[3] == board[7] and board[3] != ' '):
+        return True
 
-
-
+    return False
 
 
 
